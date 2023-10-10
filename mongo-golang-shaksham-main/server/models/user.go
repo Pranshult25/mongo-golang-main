6c1211d29948e3e3ff81421fe54bc006cf0413d7
Shreyansh Singh <shreyanshs22@iitk.ac.in>
2023-10-10 15:29:37 +0530

    Update user.go

--- mongo-golang-shaksham-main/server/models/user.go
+++ mongo-golang-shaksham-main/server/models/user.go
@@ -10,6 +10,7 @@ type User struct {
 	Email    string             `json:"email" bson:"email"`
 	Username string             `json:"username" bson:"username"`
 	Password string             `json:"password" bson:"password"`
+	Role     string             `json:"role" bson:"role"`
 }
 
 type Credentials struct {
